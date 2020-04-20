c9982338521e8f5076fe7f13c07b555e1403dca6
Angel Pereira <angel.sp06@gmail.com>
2020-04-19 22:07:30 -0300

    feat: init API
    developing rest api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,74 @@ app.use(cors());
 
 const repositories = [];
 
+const body = {
+  title,
+  url,
+  techs
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  let body = { id: uuid(), title, url, techs, likes: 0 }
+  
+  repositories.push(body)
+
+  return response.json(body)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs} = request.body
+      
+  const found = repositories.findIndex((body) => body.id == id)
+
+  if(found < 0){
+    return repositories.status(400).json({ error: 'Repository not found :('})
+  }
+
+  let body = {
+    title,
+    url,
+    techs
+  }
+
+  repositories[found] = body
+
+  return repositories.json(body)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+      
+  const found = repositories.findIndex(body => body.id == id)
+
+  if(found < 0){
+    return repositories.status(400).json({ error: 'Repository not found :('})
+  }
+
+  repositories.splice(found, 1)
+
+  return repositories.status(200)
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const found = repositories.findIndex(body => body.id == id)
+
+  if(found < 0){
+    return repositories.status(400).json({ error: 'Repository not found :('})
+  }
+  
+  const like = like + 1
+  repositories[found] = body.like
+
+  return repositories.json(body)
+
 });
 
 module.exports = app;
